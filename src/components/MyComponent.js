2ec1ea7d921fa0cd95d87d40dfc988d26ce6c97a
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2020-11-03 23:01:36 +0000

    component-test

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,10 +4,10 @@ import PropTypes from "prop-types";
 const MyComponent = ({ itemOne,itemTwo}) => {
   return (
     <>
-      <h2>This is a dummy Component</h2>
-      <h3>This is the value of propertyA</h3>
+      <h2 style={{textAlign:'center'}}>This is a dummy Component</h2>
+      <h3 style={{textAlign:'center'}}>This is the value of propertyA</h3>
       { itemOne }
-      <h3>This is the value of propertyB</h3>
+      <h3 style={{textAlign:'center'}}>This is the value of propertyB</h3>
       { itemTwo }
     </>
   );
