5b55d31f394eeea38ca97fab4567b3912ecb9014
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-01-18 15:56:37 +0000

    component for slack and pr

---
+++ src/components/AComponent.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import RandomComponent from "./ComponentHookExample";
+
+export default {
+  component: RandomComponent,
+  title: "Pirates Galore",
+};
+
+export const Default = () => <RandomComponent />;
---
+++ src/components/ComponentHookExample.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RandomComponent() {
+  return <h2>Thar she blows</h2>;
+}
